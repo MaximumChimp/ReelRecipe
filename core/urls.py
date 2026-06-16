f48ed0a0f1cdd35d78f57709693d3bec97911818
MaximumChimp <arvincabrera37@gmail.com>
2026-06-16 15:52:09 +0800

    Add ads.txt

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.views.generic import TemplateView
 urlpatterns = [
     path('',views.home_view,name='home'),
     path('privacy-policy/',views.privacy_policy, name="privacy-policy"),
-    path('terms-of-service/',views.terms_of_service,name="terms-of-service")
+    path('terms-of-service/',views.terms_of_service,name="terms-of-service"),
+    path(
+        'ads.txt', 
+        TemplateView.as_view(template_name="ads.txt", content_type="text/plain")
+    ),
 ]
